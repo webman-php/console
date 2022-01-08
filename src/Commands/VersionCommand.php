a0c3aa748aa0aa53916b3ab64ce754790c02c9cd
walkor <walkor@workerman.net>
2022-01-08 20:02:42 +0800

    add route:list version

---
+++ src/Commands/VersionCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Webman\Console\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Helper\Table;
+use Webman\Route;
+
+class VersionCommand extends Command
+{
+    protected static $defaultName = 'version';
+    protected static $defaultDescription = 'Show webman version';
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $installed_file = base_path() . '/vendor/composer/installed.php';
+        if (is_file($installed_file)) {
+            $version_info = include $installed_file;
+        }
+        $webman_version = $version_info['root']['pretty_version'] ?? (defined('WEBMAN_VERSION') ? WEBMAN_VERSION : '< 1.2.0');
+        if ($webman_version === 'dev-master') {
+            $webman_version .= '('.(defined('WEBMAN_VERSION') ? WEBMAN_VERSION : '<1.2.0').')';
+        }
+        $output->writeln("Webman $webman_version");
+        $webman_framework_version = $version_info['versions']['workerman/webman-framework']['pretty_version'] ?? '';
+        $output->writeln("Webman-framework $webman_framework_version");
+        return self::SUCCESS;
+    }
+}
