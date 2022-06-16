94f8c6758b6e36a1c4976ccb6b144230ae47848e
walkor <walkor@users.noreply.github.com>
2022-06-16 14:50:06 +0800

    Update PluginExportCommand.php

--- src/Commands/PluginExportCommand.php
+++ src/Commands/PluginExportCommand.php
@@ -113,13 +113,13 @@ class Install
     {
         foreach (static::\$pathRelation as \$source => \$dest) {
             if (\$pos = strrpos(\$dest, '/')) {
-                \$parent_dir = base_path().'/'.substr(\$dest, 0, \$pos);
+                \$parent_dir = base_path() . '/' . substr(\$dest, 0, \$pos);
                 if (!is_dir(\$parent_dir)) {
                     mkdir(\$parent_dir, 0777, true);
                 }
             }
-            //symlink(__DIR__ . "/\$source", base_path()."/\$dest");
-            copy_dir(__DIR__ . "/\$source", base_path()."/\$dest");
+            //symlink(__DIR__ . "/\$source", base_path() . "/\$dest");
+            copy_dir(__DIR__ . "/\$source", base_path() . "/\$dest");
             echo "Create \$dest\r\n";
         }
     }
@@ -131,7 +131,7 @@ class Install
     public static function uninstallByRelation()
     {
         foreach (static::\$pathRelation as \$source => \$dest) {
-            \$path = base_path()."/\$dest";
+            \$path = base_path() . "/\$dest";
             if (!is_dir(\$path) && !is_file(\$path)) {
                 continue;
             }
