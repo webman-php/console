a3491d00d2e74ad8466e6e3ebb997adb7958d37f
walkor <walkor@users.noreply.github.com>
2022-09-19 14:37:35 +0800

    Install.php for app-plugin

--- src/Commands/AppPluginCreateCommand.php
+++ src/Commands/AppPluginCreateCommand.php
@@ -62,11 +62,13 @@ class AppPluginCreateCommand extends Command
         $this->mkdir("$base_path/plugin/$name/app/view/index", 0777, true);
         $this->mkdir("$base_path/plugin/$name/config", 0777, true);
         $this->mkdir("$base_path/plugin/$name/public", 0777, true);
+        $this->mkdir("$base_path/plugin/$name/api", 0777, true);
         $this->createFunctionsFile("$base_path/plugin/$name/app/functions.php");
         $this->createControllerFile("$base_path/plugin/$name/app/controller/IndexController.php", $name);
         $this->createViewFile("$base_path/plugin/$name/app/view/index/index.html");
         $this->createExceptionFile("$base_path/plugin/$name/app/exception/Handler.php", $name);
         $this->createConfigFiles("$base_path/plugin/$name/config", $name);
+        $this->createApiFiles("$base_path/plugin/$name/api", $name);
     }
 
     /**
@@ -201,6 +203,115 @@ EOF;
      * @param $name
      * @return void
      */
+    protected function createApiFiles($base, $name)
+    {
+        $content = <<<EOF
+<?php
+
+namespace plugin\\$name\api;
+
+use plugin\admin\api\Menu;
+
+class Install
+{
+    /**
+     * 安装
+     *
+     * @param \$version
+     * @return void
+     */
+    public static function install(\$version)
+    {
+        // 导入菜单
+        Menu::import(static::getMenus());
+    }
+
+    /**
+     * 卸载
+     *
+     * @param \$version
+     * @return void
+     */
+    public static function uninstall(\$version)
+    {
+        // 删除菜单
+        foreach (static::getMenus() as \$menu) {
+            Menu::delete(\$menu['name']);
+        }
+    }
+
+    /**
+     * 更新
+     *
+     * @param \$from_version
+     * @param \$to_version
+     * @param \$context
+     * @return void
+     */
+    public static function update(\$from_version, \$to_version, \$context = null)
+    {
+        // 删除不用的菜单
+        if (isset(\$context['previous_menus'])) {
+            static::removeUnnecessaryMenus(\$context['previous_menus']);
+        }
+        // 导入新菜单
+        Menu::import(static::getMenus());
+    }
+
+    /**
+     * 更新前数据收集等
+     *
+     * @param \$from_version
+     * @param \$to_version
+     * @return array|array[]
+     */
+    public static function beforeUpdate(\$from_version, \$to_version)
+    {
+        // 在更新之前获得老菜单，通过context传递给 update
+        return ['previous_menus' => static::getMenus()];
+    }
+
+    /**
+     * 获取菜单
+     *
+     * @return array|mixed
+     */
+    public static function getMenus()
+    {
+        clearstatcache();
+        if (is_file(\$menu_file = __DIR__ . '/../config/menu.php')) {
+            \$menus = include \$menu_file;
+            return \$menus ?: [];
+        }
+        return [];
+    }
+
+    /**
+     * 删除不需要的菜单
+     *
+     * @param \$previous_menus
+     * @return void
+     */
+    public static function removeUnnecessaryMenus(\$previous_menus)
+    {
+        \$menus_to_remove = array_diff(Menu::column(\$previous_menus, 'name'), Menu::column(static::getMenus(), 'name'));
+        foreach (\$menus_to_remove as \$name) {
+            Menu::delete(\$name);
+        }
+    }
+
+}
+EOF;
+
+        file_put_contents("$base/Install.php", $content);
+
+    }
+
+    /**
+     * @param $base
+     * @param $name
+     * @return void
+     */
     protected function createConfigFiles($base, $name)
     {
         // app.php
@@ -217,6 +328,15 @@ return [
 
 EOF;
         file_put_contents("$base/app.php", $content);
+
+        // menu.php
+        $content = <<<EOF
+<?php
+
+return [];
+
+EOF;
+        file_put_contents("$base/menu.php", $content);
 
         // autoload.php
         $content = <<<EOF
