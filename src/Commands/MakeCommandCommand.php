cc5056b773e2f0379933725f9f111ab4f1b0d255
walkor <walkor@users.noreply.github.com>
2023-12-04 17:21:24 +0800

    Update MakeCommandCommand.php

--- src/Commands/MakeCommandCommand.php
+++ src/Commands/MakeCommandCommand.php
@@ -100,7 +100,7 @@ class $name extends Command
      * @param OutputInterface \$output
      * @return int
      */
-    protected function execute(InputInterface \$input, OutputInterface \$output)
+    protected function execute(InputInterface \$input, OutputInterface \$output): int
     {
         \$name = \$input->getArgument('name');
         \$output->writeln('Hello $command');
