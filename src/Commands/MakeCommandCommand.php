5d7d1e64f17ab336b5941b9fe6866207f43c94dd
落月 <1569097443@qq.com>
2025-05-05 22:56:07 +0800

    feat(console): 新增make:command生成的注解

--- src/Commands/MakeCommandCommand.php
+++ src/Commands/MakeCommandCommand.php
@@ -86,17 +86,15 @@ class MakeCommandCommand extends Command
 namespace $namespace;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 
-
+#[AsCommand('$command', '$desc')]
 class $name extends Command
 {
-    protected static \$defaultName = '$command';
-    protected static \$defaultDescription = '$desc';
-
     /**
      * @return void
      */
